a0b7b97134903ca8e155e8540c91b03e9dd1cabb
witekdev <witekdev@gmail.com>
2022-09-15 11:05:33 -0500

    remove get_selected_products aux method for now

--- src/jazkarta/easyformplugin/jazshop/vocabs.py
+++ src/jazkarta/easyformplugin/jazshop/vocabs.py
@@ -19,19 +19,3 @@ def get_available_products_vocab(context):
         ))
     return SimpleVocabulary(terms)
 directlyProvides(get_available_products_vocab, IVocabularyFactory)
-
-
-def get_selected_products(context, value):
-    selected = []
-    products = context.portal_catalog(
-        object_provides=IProduct.__identifier__,
-        sort_on='sortable_title',
-        sort_order='ascending')
-    product_dict = {product.UID: ("{}|{}".format(product.Title, product.UID), "$" + str(product.getObject().price) + ' - ' + product.Title) for product in products}
-    for product in value:
-        if product in product_dict:
-            selected.append(product_dict[product])
-    selected = sorted(selected, key=lambda(x): float(x[1].split()[0][1:]))
-    if context.portal_type == 'JazShopSelectStringField' and context.fgDefault:
-        selected.insert(0, ('{}|0'.format(context.fgDefault), context.fgDefault))
-    return selected
