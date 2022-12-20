ee7aef873ba35cd3d9ef2387dbdd5407f4d02363
witekdev <witekdev@gmail.com>
2022-12-20 14:54:40 -0500

    delete old commented out code

--- src/jazkarta/easyformplugin/jazshop/action.py
+++ src/jazkarta/easyformplugin/jazshop/action.py
@@ -62,7 +62,7 @@ class JazShopCheckout(Action):
         return cart
 
     def onSuccess(self, fields, request):
-        """ TODO
+        """ TODO - add description
         """
         form = self.get_form()
         cart = self.get_cart(request)
@@ -93,87 +93,6 @@ class JazShopCheckout(Action):
         cart.data['easyform_forms'][form_uid] = cart_fields
         cart.data['easyform_details'][form_uid] = details
         update_cart_data(cart)  # Will also save the cart for us
-
-        # XXX Test this, see whats required and what is not
-        """
-        cart = Cart.from_request(REQUEST)
-        products = []
-        pfg_products = []
-        arbitrary = []
-        if 'pfg_products' not in cart.data:
-            cart.data['pfg_products'] = {}
-        if 'pfg_details' not in cart.data:
-            cart.data['pfg_details'] = {}
-        if 'pfg_forms' not in cart.data:
-            cart.data['pfg_forms'] = {}
-        for field in fields:
-            if field.portal_type in JAZSHOP_FIELDS:
-                value = REQUEST.form.get(field.id)
-                if not value:
-                    continue
-                if isinstance(value, list):
-                    value = map(lambda x: x.split('|')[-1], value)
-                    products.extend(value)
-                else:
-                    value = value.split('|')[-1]
-                    products.append(value)
-            if field.portal_type == 'JazShopArbitraryPriceStringField':
-                price = REQUEST.form.get(field.id)
-                if not price or not field.availableProducts:
-                    continue
-                product_value = field.availableProducts[0]
-                product_uid = product_value.split('|')[-1]
-                cart.add_product(product_uid)
-                arbitrary.append(product_uid)
-                for item in cart._items.values():
-                    if item['uid'] == product_uid:
-                        price = price.replace('$', '')
-                        item['price'] = Decimal(price)
-        for uid in products:
-            if uid != '0':
-                cart.add_product(uid)
-                pfg_products.append(uid)
-        if item_prepend is not None:
-            for item in cart._items.values():
-                if (item['uid'] in (products + arbitrary) and
-                        not item['name'].startswith(item_prepend)):
-                    item['name'] = item_prepend + item['name']
-        # store form fields and reference to this form
-        pfg_form = self.aq_parent
-        pfg_form_uid = self.aq_parent.UID()
-        cart.data['pfg_products'][pfg_form_uid] = pfg_products
-        fields, details = self._get_item_details(pfg_form, REQUEST)
-        cart.data['pfg_forms'][pfg_form_uid] = fields
-        cart.data['pfg_details'][pfg_form_uid] = details
-        order_details = ''
-        cart_products = [i.uid for i in cart.items]
-        for form_uid in cart.data['pfg_forms'].keys():
-            form_products = cart.data['pfg_products'][form_uid]
-            in_cart = True
-            for p in form_products:
-                if p not in cart_products:
-                    in_cart = False
-            if in_cart:
-                order_details += cart.data['pfg_details'][form_uid]
-        cart.data['order_details'] = order_details
-        cart.save()
-
-
-        def get_selected_products(context, value):
-            selected = []
-            products = context.portal_catalog(
-                object_provides=IProduct.__identifier__,
-                sort_on='sortable_title',
-                sort_order='ascending')
-            product_dict = {product.UID: ("{}|{}".format(product.Title, product.UID), "$" + str(product.getObject().price) + ' - ' + product.Title) for product in products}
-            for product in value:
-                if product in product_dict:
-                    selected.append(product_dict[product])
-            selected = sorted(selected, key=lambda(x): float(x[1].split()[0][1:]))
-            if context.portal_type == 'JazShopSelectStringField' and context.fgDefault:
-                selected.insert(0, ('{}|0'.format(context.fgDefault), context.fgDefault))
-            return selected
-        """
 
 
 def uids_to_title(values):
